54f8502e732ccebe0d2f69bff75c5ef706661e5d
anascassa <anacaroline25190@gmail.com>
2026-02-05 19:23:08 -0300

    MVC

---
+++ projeto9/src/App.java
@@ -0,0 +1,10 @@
+import ui.Ui;
+
+public class App {
+
+	public static void main(String[] args) {
+		Ui ui = new Ui();
+		ui.run();
+	}
+
+}
---
+++ projeto9/src/repository/FilmeRepository.java
@@ -0,0 +1,26 @@
+package repository;
+
+import java.util.HashSet;
+
+import models.entities.Filme;
+
+public class FilmeRepository {
+
+	private HashSet<Filme> filmes;
+	
+	public FilmeRepository() {
+		this.filmes = new HashSet<Filme>();
+	}
+	
+	public boolean addFilme(Filme filme) {	
+		return this.filmes.add(filme);
+	}
+	
+	public boolean removeFilme(Filme filme) {
+		return this.filmes.remove(filme);
+	}
+	
+	public HashSet<Filme> getFilmes(){
+		return new HashSet<Filme>(this.filmes);
+	}
+}
